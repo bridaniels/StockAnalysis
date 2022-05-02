c9d7dcc7910c615be67c99c0a6f34c12155efa44
Brighton Code <brighton.m.daniels@gmail.com>
2022-05-02 16:27:19 -0400

    VAR edits

--- FAANG_analysis/FAANGdata.py
+++ FAANG_analysis/FAANGdata.py
@@ -9,15 +9,19 @@ ticker = 'FB'
 #ticker = 'NFLX'
 #ticker = 'GOOG'
 
-path = "/Users/CODEUSR/Documents/PROJECTS/finance/StockAnalysis/FAANG_analysis/data/stocks"
-files = os.listdir(path)
+#path = "/Users/CODEUSR/Documents/PROJECTS/finance/StockAnalysis/FAANG_analysis/data/stocks"
+#files = os.listdir(path)
+
+path = os.getcwd()+'/FAANG_analysis/'
+
+
+files = os.listdir(path+'data/stocks')
 stocks = {}
-for file in files: 
+for file in files:
     if file.split('.')[1] == 'csv':
         name = file.split('.')[0]
         stocks[name] = pd.read_csv('data/stocks/'+file, index_col='Date')
         stocks[name].index = pd.to_datetime(stocks[name].index)
-
-print('List of stocks: ')
-for i in stocks.keys(): 
-    print(i.uper(),end=' ')
+print('List of stocks: ', end = ' ')
+for i in stocks.keys():
+    print(i.upper(), end=' ')
